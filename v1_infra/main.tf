72e72ec7914baa89a8ddee6146da0822cedd5d9b
EC2 Default User <ec2-user@ip-172-31-23-81.ap-northeast-1.compute.internal>
2023-02-13 14:33:23 +0000

    main.tf

---
+++ v1_infra/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-github-action-s3-hiroz"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
