7d54461c5c39213ba684a40105fc9a3dad2c7120
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2023-04-02 14:14:39 +0000

    セキュリティーグループとサブネットグループの指定がrdsになかったので追加

--- v1_infra/rds.tf
+++ v1_infra/rds.tf
@@ -3,15 +3,15 @@
 #--------------------------------------------------------------
 
 resource "aws_db_instance" "default" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t3.micro"
-  name                 = "mydb"
-  username             = "foo"
-  password             = "foobarbaz"
-  parameter_group_name = "default.mysql8.0"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  name                   = "mydb"
+  username               = "foo"
+  password               = "foobarbaz"
+  parameter_group_name   = "default.mysql8.0"
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.rds-sg.id]
   db_subnet_group_name   = aws_db_subnet_group.rds-subnet-group.name
