a639b8cfe0b635e7d19e0df00298cca379c42695
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2023-04-02 13:28:05 +0000

    お金がかかるので一旦コメントアウトしておく

--- v1_infra/dynamodb.tf
+++ v1_infra/dynamodb.tf
@@ -1,32 +1,32 @@
-resource "aws_dynamodb_table" "arexa_list" {
-  name         = "${var.prefix}_arexa_list"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "arexaId"
-  attribute {
-    name = "arexaId"
-    type = "S"
-  }
-}
+# resource "aws_dynamodb_table" "arexa_list" {
+#   name         = "${var.prefix}_arexa_list"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "arexaId"
+#   attribute {
+#     name = "arexaId"
+#     type = "S"
+#   }
+# }
 
-resource "aws_dynamodb_table_item" "arexa_list_item" {
-  table_name = aws_dynamodb_table.arexa_list.name
-  hash_key   = "arexaId"
-  item = jsonencode({
-    arexaId = {
-      S = "a00000110"
-    },
-    FirstName = {
-      S = "Taro"
-    },
-    LastName = {
-      S = "Momo"
-    },
-    Office = {
-      S = "Nagoya"
-    }
-  })
-}
+# resource "aws_dynamodb_table_item" "arexa_list_item" {
+#   table_name = aws_dynamodb_table.arexa_list.name
+#   hash_key   = "arexaId"
+#   item = jsonencode({
+#     arexaId = {
+#       S = "a00000110"
+#     },
+#     FirstName = {
+#       S = "Taro"
+#     },
+#     LastName = {
+#       S = "Momo"
+#     },
+#     Office = {
+#       S = "Nagoya"
+#     }
+#   })
+# }
 
-output "arexa_list_table" {
-  value = aws_dynamodb_table.arexa_list
-}
+# output "arexa_list_table" {
+#   value = aws_dynamodb_table.arexa_list
+# }
