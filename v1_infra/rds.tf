7af812ad7a4242a3bf0f1b3061c579baa4ef42c2
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2023-04-02 14:05:14 +0000

    rdsバージョン違いによるバグ修正

--- v1_infra/rds.tf
+++ v1_infra/rds.tf
@@ -3,14 +3,15 @@
 #--------------------------------------------------------------
 
 resource "aws_db_instance" "default" {
-  allocated_storage    = 10
+  allocated_storage    = 20
+  storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "8.0"
   instance_class       = "db.t3.micro"
+  name                 = "mydb"
   username             = "foo"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql8.0"
-  skip_final_snapshot  = true
 }
 
 
