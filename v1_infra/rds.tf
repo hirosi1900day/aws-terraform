a4ec168dc48c9c9d6bd1ed7745e077bcbae321f0
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2023-04-03 23:20:31 +0000

    rdsコメントアウト

--- v1_infra/rds.tf
+++ v1_infra/rds.tf
@@ -1,58 +1,58 @@
-#--------------------------------------------------------------
-# RDS 本当はkmsからpassword取得しないといけないよ！
-#--------------------------------------------------------------
+# #--------------------------------------------------------------
+# # RDS 本当はkmsからpassword取得しないといけないよ！
+# #--------------------------------------------------------------
 
-resource "aws_db_instance" "default" {
-  allocated_storage      = 20
-  storage_type           = "gp2"
-  engine                 = "mysql"
-  engine_version         = "8.0"
-  instance_class         = "db.t3.micro"
-  name                   = "mydb"
-  username               = "foo"
-  password               = "foobarbaz"
-  parameter_group_name   = "default.mysql8.0"
-  skip_final_snapshot    = true
-  multi_az               = true
-  vpc_security_group_ids = [aws_security_group.rds-sg.id]
-  db_subnet_group_name   = aws_db_subnet_group.rds-subnet-group.name
-}
+# resource "aws_db_instance" "default" {
+#   allocated_storage      = 20
+#   storage_type           = "gp2"
+#   engine                 = "mysql"
+#   engine_version         = "8.0"
+#   instance_class         = "db.t3.micro"
+#   name                   = "mydb"
+#   username               = "foo"
+#   password               = "foobarbaz"
+#   parameter_group_name   = "default.mysql8.0"
+#   skip_final_snapshot    = true
+#   multi_az               = true
+#   vpc_security_group_ids = [aws_security_group.rds-sg.id]
+#   db_subnet_group_name   = aws_db_subnet_group.rds-subnet-group.name
+# }
 
 
-#--------------------------------------------------------------
-# Subnet group
-#--------------------------------------------------------------
+# #--------------------------------------------------------------
+# # Subnet group
+# #--------------------------------------------------------------
 
-resource "aws_db_subnet_group" "rds-subnet-group" {
-  name        = "mydb"
-  description = "rds subnet group"
-  subnet_ids  = [aws_subnet.private[0].id, aws_subnet.private[1].id]
-}
+# resource "aws_db_subnet_group" "rds-subnet-group" {
+#   name        = "mydb"
+#   description = "rds subnet group"
+#   subnet_ids  = [aws_subnet.private[0].id, aws_subnet.private[1].id]
+# }
 
-#--------------------------------------------------------------
-# Security group
-#--------------------------------------------------------------
+# #--------------------------------------------------------------
+# # Security group
+# #--------------------------------------------------------------
 
-resource "aws_security_group" "rds-sg" {
-  name        = "rds-sg"
-  description = "RDS service security group"
-  vpc_id      = aws_vpc.vpc.id
+# resource "aws_security_group" "rds-sg" {
+#   name        = "rds-sg"
+#   description = "RDS service security group"
+#   vpc_id      = aws_vpc.vpc.id
 
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 3306
+#     to_port     = 3306
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  tags = {
-    Name = "rds-sg"
-  }
-}
+#   tags = {
+#     Name = "rds-sg"
+#   }
+# }
