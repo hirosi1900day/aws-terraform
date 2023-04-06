bf3bd67a1a07a99d43f9c7790b4beda7e88103ab
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2023-04-06 23:53:31 +0000

    public ip自動付与を有効にする

--- v1_infra/network.tf
+++ v1_infra/network.tf
@@ -24,7 +24,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = cidrsubnet(aws_vpc.vpc.cidr_block, 8, count.index)
   availability_zone       = data.aws_availability_zones.az.names[count.index]
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "vpc-${var.name}-public-subnet-${count.index + 1}"
