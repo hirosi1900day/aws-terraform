e20ec8af82c0d2653cebe270818e688d93cc6160
hiroshi.tokudomi <hiroshi.tokudomi@medpeer.co.jp>
2023-04-25 08:59:27 +0900

    修正

--- v1_infra/iam.tf
+++ v1_infra/iam.tf
@@ -40,46 +40,46 @@ resource "aws_iam_role_policy_attachment" "tr_lambda_role_policy_attach" {
 #   })
 # }
 
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_execution_role"
+# resource "aws_iam_role" "lambda_role" {
+#   name = "lambda_execution_role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         Effect = "Allow"
+#         Principal = {
+#           Service = "lambda.amazonaws.com"
+#         }
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy" "lambda_policy" {
-  name        = "lambda_execution_policy"
-  role = aws_iam_role.tr_lambda_role.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = "s3:GetObject"
-        Resource = "arn:aws:s3:::my-bucket/*"
-      }
-    ]
-  })
-}
+# resource "aws_iam_role_policy" "lambda_policy" {
+#   name = "lambda_execution_policy"
+#   role = aws_iam_role.tr_lambda_role.id
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "logs:CreateLogGroup",
+#           "logs:CreateLogStream",
+#           "logs:PutLogEvents"
+#         ]
+#         Resource = "*"
+#       },
+#       {
+#         Effect   = "Allow"
+#         Action   = "s3:GetObject"
+#         Resource = "arn:aws:s3:::my-bucket/*"
+#       }
+#     ]
+#   })
+# }
 
 # resource "aws_iam_role_policy_attachment" "lambda_role_policy" {
 #   policy_arn = aws_iam_role_policy.lambda_policy.id
