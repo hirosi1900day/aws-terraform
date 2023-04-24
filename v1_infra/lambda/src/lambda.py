459fbd2b4d6b78cb0428f9663fc727f2d34da534
hiroshi.tokudomi <hiroshi.tokudomi@medpeer.co.jp>
2023-04-24 09:25:38 +0900

    lambada反映してみる

--- v1_infra/lambda/src/lambda.py
+++ v1_infra/lambda/src/lambda.py
@@ -1,22 +1,42 @@
-import os
-import json
 import boto3
+import json
+import logging
+import os
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table(os.getenv('TABLE_NAME'))
+from base64 import b64decode
+from urllib.request import Request, urlopen
+from urllib.error import URLError, HTTPError
 
-def handler(event, context):
-    employee_info = table.get_item(Key={'EmployeeId': 'a00000110'})['Item']
-    employee_id = employee_info['EmployeeId']
-    employee_firstname = employee_info['FirstName']
-    employee_lastname = employee_info['LastName']
-    employee_office = employee_info['Office']
-    return {
-        'statusCode': 200,
-        'body': json.dumps({
-            'Id': employee_id,
-            'Firstname': employee_firstname,
-            'Lastname': employee_lastname,
-            'Office': employee_office,
-        })
+
+SLACK_CHANNEL = os.environ['SLACK_CHANEL']
+
+ENCRYPTED_WEB_HOOK_URL = os.environ['WEB_HOOK_URL']
+HOOK_URL = boto3.client('kms').decrypt(
+        CiphertextBlob=b64decode(ENCRYPTED_WEB_HOOK_URL),
+        EncryptionContext={'LambdaFunctionName': os.environ['AWS_LAMBDA_FUNCTION_NAME']}
+    )['Plaintext'].decode('utf-8')
+    
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    print('DECRYPTED : {}'.format(HOOK_URL))
+    slack_message = {
+        'channel': SLACK_CHANNEL,
+        'attachments':  [{
+            'title': 'DBでエラーが発生しました',
+            'text': 'DBでエラーが発生しました'    
+        }]
     }
+
+    req = Request(HOOK_URL, json.dumps(slack_message).encode('utf-8'))
+    
+    try:
+        response = urlopen(req)
+        response.read()
+        logger.info("Message posted to %s", slack_message['channel'])
+    except HTTPError as e:
+        logger.error("Request failed: %d %s", e.code, e.reason)
+    except URLError as e:
+        logger.error("Server connection failed: %s", e.reason)
