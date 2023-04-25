86a184cf89d669234ad8f22cccb5886e56fb93fc
hiroshi.tokudomi <hiroshi.tokudomi@medpeer.co.jp>
2023-04-26 08:55:42 +0900

    kms不要なもの削除

--- v1_infra/kms.tf
+++ v1_infra/kms.tf
@@ -5,13 +5,6 @@ resource "aws_kms_key" "demo" {
   deletion_window_in_days = 7
 }
 
-resource "aws_kms_key" "demo" {
-  description             = "for demo"
-  key_usage               = "ENCRYPT_DECRYPT"
-  enable_key_rotation     = true
-  deletion_window_in_days = 7
-}
-
 resource "aws_kms_alias" "demo" {
   name          = "alias/demo-alias"
   target_key_id = aws_kms_key.demo.id
