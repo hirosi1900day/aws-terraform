5d2afcd803cb65f9d4ad19ba5c3c14719e078446
hiroshi.tokudomi <hiroshi.tokudomi@medpeer.co.jp>
2023-04-25 09:01:40 +0900

    修正

--- v1_infra/lambda.tf
+++ v1_infra/lambda.tf
@@ -21,7 +21,7 @@ resource "aws_lambda_function" "tr_lambda" {
 
 resource "aws_cloudwatch_log_subscription_filter" "my_subscription_filter" {
   name            = "my_subscription_filter"
-  role_arn        = aws_iam_role.lambda_role.arn
+  role_arn        = aws_iam_role.lambda_role.id
   log_group_name  = "/aws/rds/instance/database-1/error"
   filter_pattern  = "ERROR"
   destination_arn = "arn:aws:lambda:ap-northeast-1:572919087216:function:sample1_tr_lambda"
