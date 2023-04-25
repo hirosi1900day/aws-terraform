d9c8e662835879ec24a65a558ca664432aa87661
hiroshi.tokudomi <hiroshi.tokudomi@medpeer.co.jp>
2023-04-26 08:52:27 +0900

    不要なもの削除

--- v1_infra/kms.tf
+++ v1_infra/kms.tf
@@ -5,10 +5,6 @@ resource "aws_kms_key" "demo" {
   deletion_window_in_days = 7
 }
 
-
-9
-10
-11
 resource "aws_kms_key" "demo" {
   description             = "for demo"
   key_usage               = "ENCRYPT_DECRYPT"
