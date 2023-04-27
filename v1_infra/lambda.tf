2d22e86fb45cf4c24e00a02e76de89b6cf16b07b
hiroshi.tokudomi <hiroshi.tokudomi@medpeer.co.jp>
2023-04-27 09:06:40 +0900

    ハンドラー変更

--- v1_infra/lambda.tf
+++ v1_infra/lambda.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "tr_lambda" {
   filename         = data.archive_file.tr_lambda.output_path
   function_name    = "${var.prefix}_tr_lambda"
   role             = aws_iam_role.tr_lambda_role.arn
-  handler          = "tr_lambda.handler"
+  handler          = "lambda_function.lambda_handler"
   source_code_hash = data.archive_file.tr_lambda.output_base64sha256
   runtime          = "python3.9"
   timeout          = 29
